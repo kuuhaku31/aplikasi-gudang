9537f24f2e1e5f28140ad60cd6752f170c6217bc
kuuhaku31 <mageart567@gmail.com>
2020-09-01 10:07:50 +0700

    create data

--- app/views/barang/index.php
+++ app/views/barang/index.php
@@ -22,12 +22,12 @@
             | dan list dari nama barang yang sudah ada di database 
             -->
 
-            <!-- Button trigger modal -->
-            <button type="button" class="btn btn-primary btn-sm tambahData" data-toggle="modal" data-target="#tambahData" style="position: absolute; right: 15px">
-                Tambah data
-            </button>
-
-            <h3>Daftar Barang</h3>
+            <div class="header">
+                <a href="" class="float-right">
+                    <i class="fa fa-plus-circle fa-2x"></i>
+                </a>
+                <h3>Daftar Barang</h3>
+            </div>
 
             <!-- 
             | bagian ini berisi nama barang dan beberapa tombol untuk  
@@ -36,7 +36,7 @@
             -->
 
             <!-- list nama barang -->
-            <table class="table">
+            <table class="table mt-4">
                 <thead>
                     <tr>
                         <th>No.</th>
@@ -56,7 +56,6 @@
                                 <?= $num++; ?>
                             </td>
                             <td>
-
                                 <!-- 
                                 | bagian ini berisi nama barang dan hanya di tulis sekali saja
                                 | karena bagian ini ada di dalam tag perulangan php yaitu
@@ -93,50 +92,3 @@
 | bagian ini adalah modal box yang nantinya akan digunakan  
 | untuk input dan edit data pada database
 -->
-<div class="modal fade" id="tambahData" tabindex="-1" role="dialog" aria-labelledby="modalTitle" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="modalHeader">Tambah Barang</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form action="<?= BASEURL; ?>/barang/tambah" method="POST">
-                    <input type="hidden" name="id" id="id">
-                    <div class="form-group">
-                        <label for="nama">Nama</label>
-                        <input type="text" class="form-control" id="nama" name="nama" autocomplete="off">
-                    </div>
-                    <div class="form-row">
-                        <div class="form-group col-md-6">
-                            <label for="tem_lahir">Tempat lahir</label>
-                            <input type="text" class="form-control" name="tem_lahir" id="tempatlahir" autocomplete="off">
-                        </div>
-                        <div class="form-group col-md-6">
-                            <label for="tan_lahir">Tanggal lahir</label>
-                            <input type="text" class="form-control datepicker" name="tan_lahir" id="tanggallahir" autocomplete="off">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="alamat">Alamat</label>
-                        <textarea class="form-control" id="alamat" rows="3" name="alamat"></textarea>
-                    </div>
-                    <div class="form-group">
-                        <label for="jurusan">Jurusan</label>
-                        <select class="form-control" name="jurusan" id="jurusan">
-                            <option value="IPA">IPA</option>
-                            <option value="IPS">IPS</option>
-                            <option value="BAHASA">BAHASA</option>
-                        </select>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary" name="submit">Tambah data</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -3,6 +3,7 @@
 
 <head>
     <title>Halaman <?= $data['judul']; ?></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
     <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.css">
 </head>
 
